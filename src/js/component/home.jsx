87af0da37cf58301a56749605b40bc7cc02f819c
Garx1212 <113130182+Garx1212@users.noreply.github.com>
2024-10-17 01:29:56 +0000

    commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -11,32 +11,38 @@ const App = () => {
     }
   };
 
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      addTask();
+    }
+  };
+
   const removeTask = (index) => {
     setTasks(tasks.filter((_, i) => i !== index));
   };
 
   return (
     <div className="App">
-      <h1>todos</h1>
+      <h1 className='text-light'>Todos</h1>
       <div className="input-container">
         <input
           type="text"
           value={task}
           onChange={(e) => setTask(e.target.value)}
+          onKeyUp={handleKeyPress}
           placeholder="What needs to be done?"
         />
-        <button onClick={addTask}>Add</button>
       </div>
       <ul>
         {tasks.map((task, index) => (
           <li key={index}>
             {task}
-            <button className="delete-button" onClick={() => removeTask(index)}>X</button>
+            <button className="btn btn-danger" onClick={() => removeTask(index)}><i class="bi bi-trash"></i></button>
           </li>
         ))}
       </ul>
       <div className="footer">
-        <p>{tasks.length} item{tasks.length !== 1 ? 's' : ''} left</p>
+        <p className='text-light'>{tasks.length} item{tasks.length !== 1 ? 's' : ''} left</p>
       </div>
     </div>
   );
