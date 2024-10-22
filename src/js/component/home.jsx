453603d61c165fecf9c9afd361ac64a839fdaef6
Garx1212 <113130182+Garx1212@users.noreply.github.com>
2024-10-22 19:51:08 +0000

    ultimo commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,14 +1,22 @@
 import React, { useState } from 'react';
 
+'./App.css';
+
 const App = () => {
   const [task, setTask] = useState('');
   const [tasks, setTasks] = useState([]);
 
   const addTask = () => {
     if (task) {
-      setTasks([...tasks, task]);
+      const newTasks = [...tasks, task];
+      setTasks(newTasks);
       setTask('');
     }
+  };
+
+  const removeTask = (index) => {
+    const newTasks = tasks.filter((_, i) => i !== index);
+    setTasks(newTasks);
   };
 
   const handleKeyPress = (event) => {
@@ -17,32 +25,40 @@ const App = () => {
     }
   };
 
-  const removeTask = (index) => {
-    setTasks(tasks.filter((_, i) => i !== index));
-  };
-
   return (
-    <div className="App">
-      <h1 className='text-light'>Todos</h1>
-      <div className="input-container">
-        <input
-          type="text"
-          value={task}
-          onChange={(e) => setTask(e.target.value)}
-          onKeyUp={handleKeyPress}
-          placeholder="No hay tareas, agrega alguna?"
-        />
-      </div>
-      <ul>
-        {tasks.map((task, index) => (
-             <li key={index} onMouseEnter={(e) => e.target.classList.add('hover')} onMouseLeave={(e) => e.target.classList.remove('hover')}>
-             {task}
-             <span className="remove-btn btn btn-denger" onClick={() => removeTask(index)}>✖</span>
-         </li>
-        ))}
-      </ul>
-      <div className="footer">
-        <p className='text-light'>{tasks.length} item{tasks.length !== 1 ? 's' : ''} left</p>
+    <div className="App container">
+      <div className="card mt-5">
+        <div className="card-body">
+          <h1 className="card-title">Todos</h1>
+          <div className="input-group mb-3">
+            <input
+              type="text"
+              className="form-control"
+              value={task}
+              onChange={(e) => setTask(e.target.value)}
+              onKeyUp={handleKeyPress}
+            
+            />
+            <div className="input-group-append">
+              
+            </div>
+          </div>
+          {tasks.length === 0 ? (
+            <p className="card-text">No hay tareas, agrega una</p>
+          ) : (
+            <ul className="list-group">
+              {tasks.map((task, index) => (
+                <li key={index} className="list-group-item d-flex justify-content-between align-items-center task-item">
+                  {task}
+                  <button className="btn btn-danger delete-button" onClick={() => removeTask(index)}>X</button>
+                </li>
+              ))}
+            </ul>
+          )}
+          <div className="footer mt-3">
+            <p>{tasks.length} item{tasks.length !== 1 ? 's' : ''} left</p>
+          </div>
+        </div>
       </div>
     </div>
   );
